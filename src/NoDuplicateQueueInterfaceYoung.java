aa979ecf669fc5103db032f1c7c1404e2878a3cd
BFunk86 <youngbd01@sunyjcc.edu>
2016-10-25 14:24:18 -0400

    Added JavaDoc to the top of NoDuplicateQueueInterface.java

--- src/NoDuplicateQueueInterfaceYoung.java
+++ src/NoDuplicateQueueInterfaceYoung.java
@@ -2,6 +2,9 @@
  * CSC 1600 Data Structures
  * CH2 Programming Lab
  *
+ * The NoDuplicateQueueInterfaceYoung Interface is an interface that allows only a single copy of an object in the
+ * queue. If an object is added to the queue, but is already there then the queue is unchanged. 
+ *
  * @author Brandon Young
  */
 public interface NoDuplicateQueueInterfaceYoung<T> extends QueueInterface<T> {
